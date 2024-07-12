77ff632a4a67b4619dda2ffca7e4aa787ff45cb9
Jehnson Jesus Carcamo Rivera <jehnson.carcamo@unah.hn>
2024-07-12 10:52:00 -0600

    Add files via upload

---
+++ Interfaz/src/interfaz/Interfaz.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaz;
+
+/**
+ *
+ * @author juanalvarenga
+ */
+public class Interfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        // Instancia del Frame a mostrar en pantalla
+//        new MiPantalla().setVisible(true);
+            MiPantalla ventana = new MiPantalla(); 
+            ventana.setVisible(true); // hacer visible la ventada
+//            ventana.dispose(); // destruye de memoria la ventana
+
+    }
+
+}
